cfdbc46e2012379117c57753a749f9a29ae075dd
HightMaster <89708788+HightMaster@users.noreply.github.com>
2021-09-28 16:42:39 -0400

    Changes to create.php
    Create php was edited so it is now a form. Process.php is a test file I'm using for now until I can figure out which file will process this form data.

---
+++ create.php
@@ -0,0 +1,11 @@
+<form action="process.php" method="POST">
+	Quote<br />
+	<textarea name="quote" rows="15" cols="30" required /></textarea><br /><br />
+	Authors<br />
+	<select name="authors" id="authors" /><br /><br />
+		<option value="none" Selected>None</option>
+		<option value="munene">Munene</option>
+		<option value="sam">Sam</option>
+		<option value="elon">Elon Musk</option>
+	</select>
+</form>
