324da1047d20aa0d2befaa3603f80f51eaa0f45c
HightMaster <89708788+HightMaster@users.noreply.github.com>
2021-09-29 17:30:50 -0400

    JUst changes

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="eng">
 <?php
 
+function readCSV($csvFile){
+	$newData = $_POST;
+	$csvArray = array();
+	$handle = fopen($csvFile, 'r');
+	while (($line = fgetcsv($handle)) !== FALSE) {
+		$csvArray[] = $line;
+	}
+	
+	fclose($handle);
+	return $csvArray;
+}
 ?>
+<body>
+
+<?php $authors = readCSV('authors.csv');
+	  $authorName = "";
+	  for ($x = 0; $x < count(readCSV('quotes.csv')); $x++){ ?>
+		<?php
+			foreach($authors as $author){
+				if($author[1] == readCSV('quotes.csv')[$x][1]){
+					$authorName = $author[0];
+				}
+			}
+		?>
+		<h2><?= readCSV('quotes.csv')[$x][0]." -".$authorName;?></h2><br />
+	<?php } ?>
+</body>
+</html>
