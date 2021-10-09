eabe7005113ff3e4cb8d7f1eb035d5ed209a043d
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-10-09 11:46:10 -0400

    Update index.php
    Style changes gradually being applied to pages.

--- index.php
+++ index.php
@@ -22,21 +22,29 @@ function readCSV($csvFile){
 		<link rel="stylesheet" href="style.css">
 </head>
 <body class="b" style="font-family: Arial, sans-serif;">
-<?php $authors = readCSV('authors.csv');
-	  $authorName = "";
-	  for ($x = 0; $x < count(readCSV('quotes.csv')); $x++){ ?>
-		<?php
-		$authorElement = readCSV('quotes.csv')[$x][1];
-		$quoteIndex = $x;
-		?>
-		<p><a href="detail.php?quoteIndex=<?= $quoteIndex?>"><?= "\"".readCSV('quotes.csv')[$x][0]."\" -".returnRow($authorElement)[0]." ".returnRow($authorElement)[1];?></a></p><br />
-	<?php } ?>
-		<h2><a href="create.php">Create</a></h2>
 		<!--Top Bar-->
 		<div class="lb" style="height: 60px;"></div>
 		<!--Small Top Bar-->
 		<div class="lg" style="height: 5px;"></div>
 		<!--Body-->
 		<div class="" style="height: 0 auto; min-height: 200px; margin-bottom: 30px; padding: 30px;">
+			<h2><a href="create.php">Create</a></h2>
+			<?php $authors = readCSV('authors.csv');
+				$authorName = "";
+				for ($x = 0; $x < count(readCSV('quotes.csv')); $x++){ ?>
+					<?php
+					$authorElement = readCSV('quotes.csv')[$x][1];
+					$quoteIndex = $x;
+					?>
+					<p><a href="detail.php?quoteIndex=<?= $quoteIndex?>"><?= "\"".readCSV('quotes.csv')[$x][0]."\" -".returnRow($authorElement)[0]." ".returnRow($authorElement)[1];?></a></p><br />
+			<?php } ?>
+		
+			<!--Floating Buttons Row-->
+			<div class="row">
+				<div class="col-md-3 col-sm-5 col-xs-12 lb" style="padding: 20px; text-align: center; font-size: 20px;"><a href="create.php">Create</a></div>
+				<div class="col-md-3 col-sm-5 col-xs-12 lb">This is some text</div>
+				<div class="col-md-3 col-sm-5 col-xs-12 lb">This is some text</div>
+			</div>
+		</div>
 </body>
 </html>
