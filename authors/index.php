bba358715d5ddc05d3a64647c7f80b5af5567709
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-10-11 17:04:59 -0400

    Updated

---
+++ authors/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="eng">
+	<?php
+		require('../csv_util.php');
+	?>
+	<head>
+		<title>Great Authors - Home</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<link rel="stylesheet" href="../style1.css">
+	</head>
+	<body>
+		<!--Body Container-->
+		<div style="height: 100%;">
+			<!--Top Bar-->
+			<div class="lb" style="height: 60px;"></div>
+			<!--Small Top Bar-->
+			<div class="lg" style="height: 5px;"></div>
+			<!--Quote Column-->
+			<div class="textColumn" style="min-height: 100%;">
+				<p class="textlb" style="font-size: 100px;">Authors</p>
+				<?php $authors = returnFile('authors.csv');
+					for ($x = 0; $x < count(returnFile('authors.csv')); $x++){ ?>
+						<p><a class="otb" href="detail.php?authorIndex=<?= $x?>"><?= "".$authors[$x][0]." ".$authors[$x][1];?></a></p><br />
+				<?php } ?>
+				<!--Buttons Row-->
+				<div class="row" style="padding-top: 30px;">
+					<div class="col-md-2 col-sm-5 col-xs-12"><div class="butDiv"><a class="but" href="create.php">Create</a></div></div>
+				</div>
+			</div>
+			<!--Small Footer Bar-->
+			<div class="lg" style="height: 5px;"></div>
+			<!--Footer Bar-->
+			<div class="lb" style="height: 30px;"></div>
+		</div>
+	</body>
+</html>
